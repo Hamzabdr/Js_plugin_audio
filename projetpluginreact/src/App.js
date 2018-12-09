872605e241a539ae91f80bc781c4d4f1c55991cc
boudradar <hamza.boudradar@etu.unice.fr>
2018-12-09 17:49:51 +0100

    Import data / plugins gallery / bootstrap

--- projetpluginreact/src/App.js
+++ projetpluginreact/src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Details from './Details';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      plugins: [],
+    };
   }
+
+  componentDidMount() {
+    console.log("Will mount")
+    this.getDataFromServer();
+  }
+  getDataFromServer() {
+    fetch('http://localhost:8080/api/plugins')
+      .then(response => {
+        return response.json() // transforme le json texte en objet js
+      })
+      .then(res => { // data c'est le texte json de response ci-dessus
+        res.data.forEach((el) => {
+          if (el.name.length > 40 && el.screenshot_href == "") {
+            let plugin = {
+              name: el.name,
+              screenshot_href: el.screenshot_href
+            }
+            this.plugins.push(plugin);
+          }
+        });
+        this.setState({ plugins: res.data });
+        console.log("state: ", this.state.plugins);
+      })
+  }
+   App = () => {
+    const someVariable = true;
+  
+    return (
+      <Switch>
+        {/* these are good */}
+        <Route exact path="/" component={App} />
+        <Route
+          path="/Details"
+          render={props => <Details {...props} extra={someVariable} />}
+        />
+       
+      </Switch>
+    );
+  };
+
+  render() {
+    //let plugins = null;
+    return (
+      //<div style={{ background : "https://www.moddevices.com/hubfs/assets/billboards/home-billboard.jpg" }}>
+      <div class="back">
+        <div class="body" style={{ background: '#696969', padding: 24, minHeight: 280 }}>
+
+          <h1> Plugins gallery</h1>
+          {this.state.plugins.map((plugin, index) => {
+            return (
+              <div key={index}>
+                <div id= "wrapper" class="wrapper">
+                  <p class="name"> <bold>{plugin.name}</bold></p>
+                  <p><img alt="Not found" src={plugin.screenshot_href} /></p>
+                  <p> Categorie : {plugin.category} </p>
+                  <p>{plugin.brand}</p>
+                  <p><button type="button" class="btn btn-outline-primary">Details</button></p>
+                </div>
+              </div>
+            )
+          })
+          }
+        </div>
+      </div>
+    )
+  }
+
+
+
+
 }
+
+
 
 export default App;
---
+++ projetpluginreact/src/Config/config.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBCaU_Om2E5InqMz4wtPC-ZDx7mH9Oj99g",
+    authDomain: "reactpluginsaudio.firebaseapp.com",
+    databaseURL: "https://reactpluginsaudio.firebaseio.com",
+    projectId: "reactpluginsaudio",
+    storageBucket: "reactpluginsaudio.appspot.com",
+    messagingSenderId: "875759251157"
+  };
+  firebase.initializeApp(config);
