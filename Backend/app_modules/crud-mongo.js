8eaa19183f0335ded2ea0bfcad6b688dcf877214
boudradar <hamza.boudradar@etu.unice.fr>
2018-12-09 16:58:26 +0100

    Backend

---
+++ Backend/app_modules/crud-mongo.js
@@ -0,0 +1,202 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var assert = require('assert');
+//var url = 'mongodb://localhost:27017/test';
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'test';
+
+exports.connexionMongo = function(callback) {
+	MongoClient.connect(url, function(err, client) {
+		var db = client.db(dbName);
+		
+		assert.equal(null, err);
+		callback(err, db);
+	});
+}
+
+exports.findplugins = function(page, pagesize, callback) {
+    MongoClient.connect(url, function(err, client) {
+    	    console.log("pagesize = " + pagesize);
+			console.log("page = " + pagesize);
+			
+			var db = client.db(dbName);
+
+			console.log("db " + db)
+        if(!err){
+			db.collection('plugins')
+			.find()
+            .skip(page*pagesize)
+            .limit(pagesize)
+            .toArray()
+            .then(arr => callback(arr));
+        }
+        else{
+            callback(-1);
+        }
+    });
+};
+
+exports.findpluginById = function(id, callback) {
+    MongoClient.connect(url, function(err, client) {
+		var db = client.db(dbName);
+        if(!err) {
+        	// La requete mongoDB
+
+            let myquery = { "_id": ObjectId(id)};
+
+            db.collection("plugins") 
+            .findOne(myquery, function(err, data) {
+            	let reponse;
+
+                if(!err){
+                    reponse = {
+                    	succes: true,
+                        plugin : data,
+                        error : null,
+                        msg:"Details du plugin envoyés"
+                    };
+                } else{
+                    reponse = {
+                    	succes: false,
+                        plugin : null,
+                        error : err,
+                        msg: "erreur lors du find"
+
+                    };
+                }
+                callback(reponse);
+            });
+        } else {
+        	let reponse = reponse = {
+                    	succes: false,
+                        plugin : null,
+                        error : err,
+                        msg: "erreur de connexion à la base"
+                    };
+            callback(reponse);
+        }
+    });
+}
+
+exports.createplugin = function(formData, callback) {
+	MongoClient.connect(url, function(err, client) {
+		var db = client.db(dbName);
+
+	    if(!err) {
+	 
+			let toInsert = {
+				name : formData.nom, 
+			};
+			console.dir(JSON.stringify(toInsert));
+		    db.collection("plugins")
+		    .insertOne(toInsert, function(err, result) {
+		    	let reponse;
+
+		        if(!err){
+		            reponse = {
+		                succes : true,
+		                result: result,
+		                error : null,
+		                msg: "Ajout réussi " + result
+		            };
+		        } else {
+		            reponse = {
+		                succes : false,
+		                error : err,
+		                msg: "Problème à l'insertion"
+		            };
+		        }
+		        callback(reponse);
+		    });
+		} else{
+			let reponse = reponse = {
+                    	succes: false,
+                        error : err,
+                        msg:"Problème lors de l'insertion, erreur de connexion."
+                    };
+            callback(reponse);
+		}
+	});
+}
+
+exports.updateplugin = function(id, formData, callback) {
+
+	MongoClient.connect(url, function(err, client) {
+		var db = client.db(dbName);
+
+		if(!err) {
+            let myquery = { "_id": ObjectId(id)};
+	        let newvalues = {
+	        	name : formData.nom, 
+	        };
+
+
+			db.collection("plugins")
+			.replaceOne(myquery, newvalues, function(err, result) {
+	         	if(!err){
+			    	reponse = {
+		                succes : true,
+		                result: result,
+		                error : null,
+		                msg: "Modification réussie " + result
+		            };
+			   	} else {
+		            reponse = {
+		                succes : false,
+		                error : err,
+		                msg: "Problème à la modification"
+		            };
+			    }
+			    callback(reponse);
+	        });
+		} else{
+			let reponse = reponse = {
+                    	succes: false,
+                        error : err,
+                        msg:"Problème lors de la modification, erreur de connexion."
+                    };
+            callback(reponse);
+		}
+	});
+}
+
+exports.deleteplugin = function(id, callback) {
+	MongoClient.connect(url, function(err, client) {
+		var db = client.db(dbName);
+
+		if(!err) {
+            let myquery = { "_id": ObjectId(id)};
+	        
+			db.collection("plugins")
+			.deleteOne(myquery, function(err, result) {
+	         	if(!err){
+			    	reponse = {
+		                succes : true,
+		                result: result,
+		                error : null,
+		                msg: "Suppression réussie " + result
+		            };
+			   	} else {
+		            reponse = {
+		                succes : false,
+		                error : err,
+		                msg: "Problème à la suppression"
+		            };
+			    }
+			    callback(reponse);
+	        });
+		} else{
+			let reponse = reponse = {
+                    	succes: false,
+                        error : err,
+                        msg:"Problème lors de la suppression, erreur de connexion."
+                    };
+            callback(reponse);
+		}
+	});
+}
